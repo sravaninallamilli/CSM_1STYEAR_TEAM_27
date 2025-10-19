833ee9cc2d67dc5bd299678547dfcdc81452f932
sravaninallamilli <sravaninallamilli23@gmail.com>
2025-10-19 11:05:21 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,17 @@
+int ( " enter any number");
+{
+ 
+
+
+ if { a>=18;
+     print( " you  are eligible for the vote");
+ }
+
+ if else 
+ {    
+    a< 18;
+print( " you are not eligible for the vote");
+}
+
+
+}
